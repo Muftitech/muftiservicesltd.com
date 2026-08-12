025c34767c7c3c745b5cf2174a294d8642c68784
Muhammad bello Yusuf <muftiservicesltd@gmail.com>
2026-08-12 17:21:19 +0100

    Remove basePath and assetPrefix
    Removed basePath and assetPrefix configuration.

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 
-  basePath: "/muftiservicesltd.com",
-
-  assetPrefix: "/muftiservicesltd.com/",
-
   images: {
     unoptimized: true,
   },
